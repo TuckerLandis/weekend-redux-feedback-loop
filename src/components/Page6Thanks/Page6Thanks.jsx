650650a3723d9f34925f8dab2a99a94175f57c82
TuckerLandis <landistuckerc@gmail.com>
2021-06-12 18:16:55 -0500

    thanks

---
+++ src/components/Page6Thanks/Page6Thanks.jsx
@@ -0,0 +1,28 @@
+import { useHistory } from 'react-router-dom'
+
+function Page6Thanks() {
+    const history = useHistory();
+
+    const handleNext = () => {
+        history.push('/')
+    }
+
+    const handleHistory = () => {
+
+    }
+
+    return (
+        <div className="display">
+              <p className="prompt">
+                Thanks!
+            </p>
+        
+
+            <button onClick={handleHistory} className="prompt button">See History</button>
+
+            <button onClick={handleNext} className="prompt button">Restart</button>
+        </div>
+    )
+    }
+    
+    export default Page6Thanks
